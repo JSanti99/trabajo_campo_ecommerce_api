bc52690ed303ad15365f92d7a5f33e3be89362ad
Jeisson Santiesteban <jeisson.santiesteban@uptc.edu.co>
2021-04-07 22:16:59 -0500

    feat: productos paginacion

--- api/productos/controllers/productos.js
+++ api/productos/controllers/productos.js
@@ -1,8 +1,40 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    console.log(ctx.query);
+    let { page, perPage } = ctx.query;
+    page = page - 1;
+    delete ctx.query.page;
+    let entities;
+    let count;
+    if (ctx.query._q) {
+      entities = await strapi.services.productos.search(ctx.query);
+      count = await strapi.services.productos.count();
+    } else {
+      //   entities = await strapi.services.productos.find(ctx.query);
+      try {
+        entities = await strapi.services.productos.find();
+        entities = entities.filter(
+          (x, i) => i >= page * perPage && i < page * perPage + perPage
+        );
+      } catch (err) {
+        console.log(err);
+      }
+      count = await strapi.services.productos.count();
+    }
+    console.log(count);
+    return {
+      total: count,
+      products: entities.map((entity) =>
+        sanitizeEntity(entity, { model: strapi.models.productos })
+      ),
+    };
+  },
+};
