ee525eeba595cf7601e7fe8b3e18dbd7952ec49b
Jeisson Santiesteban <jeisson.santiesteban@uptc.edu.co>
2021-05-04 15:41:38 -0500

    epayco subs

--- api/productos/controllers/productos.js
+++ api/productos/controllers/productos.js
@@ -8,28 +8,23 @@ module.exports = {
    */
 
   async find(ctx) {
-    console.log(ctx.query);
     let { page, perPage } = ctx.query;
-    page = page - 1;
-    delete ctx.query.page;
+    page = parseInt(page);
+    perPage = parseInt(perPage);
     let entities;
     let count;
-    if (ctx.query._q) {
-      entities = await strapi.services.productos.search(ctx.query);
-      count = await strapi.services.productos.count();
-    } else {
-      //   entities = await strapi.services.productos.find(ctx.query);
-      try {
-        entities = await strapi.services.productos.find();
-        entities = entities.filter(
-          (x, i) => i >= page * perPage && i < page * perPage + perPage
-        );
-      } catch (err) {
-        console.log(err);
-      }
-      count = await strapi.services.productos.count();
+    entities = await strapi.services.productos.find();
+    try {
+      console.log({ page, perPage });
+      entities = entities.slice(
+        (page - 1) * perPage,
+        (page - 1) * perPage + perPage
+      );
+      // console.log({ entities });
+    } catch (err) {
+      console.log(err);
     }
-    console.log(count);
+    count = await strapi.services.productos.count();
     return {
       total: count,
       products: entities.map((entity) =>
